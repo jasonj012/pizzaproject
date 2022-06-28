82ac83691ffd0d37e13f0015edce68fec62457b9
jasonj012 <104796240+jasonj012@users.noreply.github.com>
2022-06-28 09:00:48 -0400

    Add files via upload

---
+++ Project2/PizzaProject/src/main/java/PizzaProject/Controller/UserController.java
@@ -0,0 +1,25 @@
+package PizzaProject.Controller;
+
+import PizzaProject.Model.UserModel;
+import PizzaProject.Service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+@RestController
+@RequestMapping("users")
+public class UserController {
+    UserService userService;
+    @Autowired
+    public UserController(UserService userService) {
+        this.userService = userService;
+    }
+    @GetMapping
+    public List<UserModel> getAllUsers() {
+        return userService.getAllUsers();
+    }
+    @PostMapping
+    public void registerUser(@RequestBody UserModel u) {
+    userService.registerUser(u);
+    }
+}
