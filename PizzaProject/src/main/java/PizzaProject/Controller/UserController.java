caeec2fb9ccb00f592281d0caa2db11e191347a8
jasonj012 <104796240+jasonj012@users.noreply.github.com>
2022-06-29 11:39:31 -0400

    Update UserController.java
    Better functioning userController

--- PizzaProject/src/main/java/PizzaProject/Controller/UserController.java
+++ PizzaProject/src/main/java/PizzaProject/Controller/UserController.java
@@ -6,13 +6,17 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.Map;
+
+@CrossOrigin(origins="*", allowedHeaders = "*")
 @RestController
 @RequestMapping("users")
 public class UserController {
     UserService userService;
+
     @Autowired
-    public UserController(UserService userService) {
-        this.userService = userService;
+    public UserController(UserService userService){
+        this.userService=userService;
     }
 
     @GetMapping
@@ -20,8 +24,26 @@ public class UserController {
         return userService.getAllUser();
     }
 
-    @PostMapping
-    public void registerUser(@RequestBody UserModel u) {
-    userService.registerUser(u);
+    @PostMapping("add")
+    public void createUser(@RequestBody UserModel user){userService.createUser(user);}
+
+    @GetMapping("{username}")
+    public UserModel getUserByUserName(@PathVariable String username){
+        return userService.getUserByUsername(username);
     }
+    @PostMapping("login")
+    public UserModel loginUser(@RequestBody Map<String, Object> dto){
+        return userService.login(dto.get("username").toString(),dto.get("password").toString());
+    }
+
+    @GetMapping("user/{id}")
+    public UserModel getUserInformation(@PathVariable("id") int id){
+        return userService.getUserInformation(id);
+    }
+
+    @GetMapping("username/{name}")
+    public UserModel getUserByName(@PathVariable("name") String name){
+        return userService.getByUsername(name);
+    }
+
 }
