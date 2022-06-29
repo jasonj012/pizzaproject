cb4b78852d0f71d4ed616bdc3dab10fc7efef205
jasonj012 <104796240+jasonj012@users.noreply.github.com>
2022-06-29 10:31:37 -0400

    Update UserDAO.java

--- PizzaProject/src/main/java/PizzaProject/DAO/UserDAO.java
+++ PizzaProject/src/main/java/PizzaProject/DAO/UserDAO.java
@@ -3,10 +3,21 @@ package PizzaProject.DAO;
 import PizzaProject.Model.UserModel;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
-
-import java.util.List;
+import org.springframework.data.repository.query.Param;
 
 public interface UserDAO extends JpaRepository<UserModel, Integer> {
-    @Query("From UserModel")
-    List<UserModel> findAllUsers();
+    @Query("From UserModel where username = :username")
+    UserModel findUserByUsername(@Param("username") String username);
+
+    @Query("from UserModel where username= :username and password= :password")
+    UserModel login(@Param("username")String username, @Param("password")String password);
+    @Query("from UserModel where username=:username")
+    UserModel getUserByUsername(@Param("username") String username);
+    @Query("from UserModel where email=:email")
+    UserModel getUserByEmail(@Param("email")String email);
+    @Query("from UserModel where id= :id")
+    UserModel getUserByID(@Param("id") int id);
+
+    UserModel findByUsername(String name);
+
 }
